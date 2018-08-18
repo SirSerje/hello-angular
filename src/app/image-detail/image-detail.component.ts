f7a2b5a8a5aba71de1382abc06382803036b79b2
SirSerje <sir.serje@gmail.com>
2018-08-18 17:15:23 +0300

    create interface for image

--- src/app/image-detail/image-detail.component.ts
+++ src/app/image-detail/image-detail.component.ts
@@ -1,17 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {Image} from "../entities/image";
-import {ActivatedRoute, Router} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
+import {Image} from '../entities/image';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+interface IIMageResponse {
+  image: Image;
+}
 
 @Component({
   selector: 'app-image-detail',
   templateUrl: './image-detail.component.html',
   styleUrls: ['./image-detail.component.css']
 })
+
 export class ImageDetailComponent implements OnInit {
-
   image: Image;
-
 
   constructor(
     private route: ActivatedRoute,
@@ -28,5 +31,4 @@ export class ImageDetailComponent implements OnInit {
       });
     });
   }
-
 }
